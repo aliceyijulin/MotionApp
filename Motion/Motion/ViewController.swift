48c81d02914dc2a7b5fa9fa628d10fdab4936c62
aliceyijulin <aliceyijulin@gmail.com>
2021-07-20 19:41:37 -0400

    BLE code added

--- Motion/Motion/ViewController.swift
+++ Motion/Motion/ViewController.swift
@@ -7,27 +7,46 @@
 
 import UIKit
 import Charts
+import Foundation
+import CoreBluetooth
 
+
+struct CBUUIDs{
+
+    static let kBLEService_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+    static let kBLE_Characteristic_uuid_Tx = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+    static let kBLE_Characteristic_uuid_Rx = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+    static let BLEService_UUID = CBUUID(string: kBLEService_UUID)
+    static let BLE_Characteristic_uuid_Tx = CBUUID(string: kBLE_Characteristic_uuid_Tx)//(Property = Write without response)
+    static let BLE_Characteristic_uuid_Rx = CBUUID(string: kBLE_Characteristic_uuid_Rx)// (Property = Read/Notify)
+
+}
 
 class ViewController: UIViewController  {
 
-
+    var centralManager: CBCentralManager!
+    private var bluefruitPeripheral: CBPeripheral!
+    private var txCharacteristic: CBCharacteristic!
+    private var rxCharacteristic: CBCharacteristic!
+    
     @IBOutlet weak var lineChartBox: LineChartView!
     @IBOutlet weak var barChartBox: BarChartView!
     @IBOutlet weak var pieChartBox: PieChartView!
     
     
-   
+    var receivedData = [Int]()
+    var showGraphIsOn = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+
         // Do any additional setup after loading the view.
         lineChartBox.isHidden = true
         barChartBox.isHidden = true
         pieChartBox.isHidden = true
+
     }
-    
         
     func graphLineChart(dataArray: [Int])
     {
@@ -168,4 +187,292 @@ class ViewController: UIViewController  {
         barChartBox.isHidden = true
         pieChartBox.isHidden = false
         }
+    
+    func startScanning() -> Void {
+      // Start Scanning
+      centralManager?.scanForPeripherals(withServices: [CBUUIDs.BLEService_UUID])
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,advertisementData: [String : Any], rssi RSSI: NSNumber) {
+
+            bluefruitPeripheral = peripheral
+            bluefruitPeripheral.delegate = self
+
+            print("Peripheral Discovered: \(peripheral)")
+            print("Peripheral name: \(peripheral.name)")
+            print ("Advertisement Data : \(advertisementData)")
+            
+            centralManager?.connect(bluefruitPeripheral!, options: nil)
+
+        }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+       bluefruitPeripheral.discoverServices([CBUUIDs.BLEService_UUID])
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+            print("*******************************************************")
+
+            if ((error) != nil) {
+                print("Error discovering services: \(error!.localizedDescription)")
+                return
+            }
+            guard let services = peripheral.services else {
+                return
+            }
+            //We need to discover the all characteristic
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            print("Discovered Services: \(services)")
+        }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+           
+               guard let characteristics = service.characteristics else {
+              return
+          }
+
+          print("Found \(characteristics.count) characteristics.")
+
+          for characteristic in characteristics {
+
+            if characteristic.uuid.isEqual(CBUUIDs.BLE_Characteristic_uuid_Rx)  {
+
+              rxCharacteristic = characteristic
+
+              peripheral.setNotifyValue(true, for: rxCharacteristic!)
+              peripheral.readValue(for: characteristic)
+
+              print("RX Characteristic: \(rxCharacteristic.uuid)")
+            }
+
+            if characteristic.uuid.isEqual(CBUUIDs.BLE_Characteristic_uuid_Tx){
+              
+              txCharacteristic = characteristic
+              
+              print("TX Characteristic: \(txCharacteristic.uuid)")
+            }
+          }
+    }
+    
+    func disconnectFromDevice () {
+        if bluefruitPeripheral != nil {
+        centralManager?.cancelPeripheralConnection(bluefruitPeripheral!)
+        }
+     }
+    
+//    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+//
+//          var characteristicASCIIValue = NSString()
+//
+//          guard characteristic == rxCharacteristic,
+//
+//          let characteristicValue = characteristic.value,
+//          let ASCIIstring = NSString(data: characteristicValue, encoding: String.Encoding.utf8.rawValue) else { return }
+//
+//        characteristicASCIIValue = ASCIIstring
+//        let str: String = ASCIIstring as String
+//        var a:Int! = Int(str)
+//
+//        data.append(a)
+//
+//        graphLineChart(dataArray: data)
+//        graphPieChart(dataArray: data)
+//        graphBarChart(dataArray: data)
+//
+//          print("Value Recieved: \((characteristicASCIIValue as String))")
+//    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic,
+                        error: Error?) {
+            
+            // If characteristic is correct, read its value and save it to a string.
+            // Else, return
+            guard characteristic == rxCharacteristic,
+            let characteristicValue = characteristic.value,
+            let receivedString = NSString(data: characteristicValue,
+                                          encoding: String.Encoding.utf8.rawValue)
+            else { return }
+            
+            for i in 0..<receivedString.length {
+                // Print for debugging purposes
+                print(receivedString.character(at: i))
+                let number:Int = Int(receivedString.character(at: i))
+                receivedData.append(number)
+            }
+            
+            if (receivedData.count > 100) {
+                receivedData.removeFirst(receivedData.count-100)
+            }
+            if (showGraphIsOn && receivedData.count > 0) {
+                displayLineGraph(dataDisplaying: receivedData)
+            }
+            if (showGraphIsOn && receivedData.count > 0) {
+                displayBarGraph(dataDisplaying: receivedData)
+            }
+            if (showGraphIsOn && receivedData.count > 0) {
+                displayPieGraph(dataDisplaying: receivedData)
+            }
+            NotificationCenter.default.post(name:NSNotification.Name(rawValue: "Notify"), object: self)
+        }
+    
+    // Graph the line chart dataset to storyboard
+       func displayLineGraph(dataDisplaying: [Int]) {
+           // Array that will eventually be displayed on the graph.
+           var lineChartEntry  = [ChartDataEntry]()
+
+           // For every element in given dataset
+           // Set the X and Y status in a data chart entry
+           // and add to the entry object
+           for i in 0..<dataDisplaying.count {
+               let value = ChartDataEntry(x: Double(i), y: Double(dataDisplaying[i]))
+               lineChartEntry.append(value)
+           }
+
+           // Convert lineChartEntry to a LineChartDataSet
+           let line1 = LineChartDataSet(entries: lineChartEntry, label: "Heart Rate Data")
+           
+           // Customize graph settings to your liking
+           line1.drawCirclesEnabled = false
+           line1.colors = ChartColorTemplates.joyful()
+
+           // Make object that will be added to the chart
+           // and set it to the variable in the Storyboard
+           let lineData = LineChartData(dataSet: line1)
+        
+           //chartBox.dragEnabled = true
+           lineChartBox.setScaleEnabled(true)
+           lineChartBox.pinchZoomEnabled = true
+           lineChartBox.data = lineData
+       }
+    
+    // Graph the bar chart dataset to storyboard
+       func displayBarGraph(dataDisplaying: [Int]) {
+           // Array that will eventually be displayed on the graph.
+           var barChartEntry  = [ChartDataEntry]()
+
+           // For every element in given dataset
+           // Set the X and Y status in a data chart entry
+           // and add to the entry object
+           for i in 0..<dataDisplaying.count {
+               let value = ChartDataEntry(x: Double(i), y: Double(dataDisplaying[i]))
+               barChartEntry.append(value)
+           }
+
+           // Convert lineChartEntry to a LineChartDataSet
+           let bar1 = BarChartDataSet(entries: barChartEntry, label: "Steps")
+           
+           // Customize graph settings to your liking
+           //bar1.drawCirclesEnabled = false
+           bar1.colors = ChartColorTemplates.joyful()
+
+           // Make object that will be added to the chart
+           // and set it to the variable in the Storyboard
+           let barData = BarChartData(dataSet: bar1)
+        
+           //chartBox.dragEnabled = true
+           barChartBox.setScaleEnabled(true)
+           barChartBox.pinchZoomEnabled = true
+           barChartBox.data = barData
+       }
+    
+    // Graph the pie chart dataset to storyboard
+       func displayPieGraph(dataDisplaying: [Int]) {
+           // Array that will eventually be displayed on the graph.
+           var pieChartEntry  = [ChartDataEntry]()
+
+           // For every element in given dataset
+           // Set the X and Y status in a data chart entry
+           // and add to the entry object
+           for i in 0..<dataDisplaying.count {
+               let value = ChartDataEntry(x: Double(i), y: Double(dataDisplaying[i]))
+               pieChartEntry.append(value)
+           }
+
+           // Convert pieChartEntry to a pieChartDataSet
+           let pie1 = PieChartDataSet(entries: pieChartEntry, label: "Report")
+           
+           // Customize graph settings to your liking
+            //pie1.drawCirclesEnabled = false
+            pie1.colors = ChartColorTemplates.joyful()
+
+           // Make object that will be added to the chart
+           // and set it to the variable in the Storyboard
+           let pieData = PieChartData(dataSet: pie1)
+        
+           //chartBox.dragEnabled = true
+           //pieChartBox.setScaleEnabled(true)
+          //pieChartBox.pinchZoomEnabled = true
+        
+           pieChartBox.data = pieData
+       }
+    
+//    func clearGraph() {
+//          let nullGraph = [Int]()
+//          displayGraph(dataDisplaying: nullGraph)
+//      }
+    
+    func writeOutgoingValue(data: String){
+          
+        let valueString = (data as NSString).data(using: String.Encoding.utf8.rawValue)
+        
+        if let bluefruitPeripheral = bluefruitPeripheral {
+              
+          if let txCharacteristic = txCharacteristic {
+                  
+            bluefruitPeripheral.writeValue(valueString!, for: txCharacteristic, type: CBCharacteristicWriteType.withResponse)
+              }
+          }
+      }
 }
+
+extension ViewController: CBCentralManagerDelegate {
+    
+  func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    
+     switch central.state {
+          case .poweredOff:
+              print("Is Powered Off.")
+          case .poweredOn:
+              print("Is Powered On.")
+              startScanning()
+          case .unsupported:
+              print("Is Unsupported.")
+          case .unauthorized:
+          print("Is Unauthorized.")
+          case .unknown:
+              print("Unknown")
+          case .resetting:
+              print("Resetting")
+          @unknown default:
+            print("Error")
+          }
+   }
+}
+
+extension ViewController: CBPeripheralManagerDelegate {
+
+  func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+    switch peripheral.state {
+    case .poweredOn:
+        print("Peripheral Is Powered On.")
+    case .unsupported:
+        print("Peripheral Is Unsupported.")
+    case .unauthorized:
+    print("Peripheral Is Unauthorized.")
+    case .unknown:
+        print("Peripheral Unknown")
+    case .resetting:
+        print("Peripheral Resetting")
+    case .poweredOff:
+      print("Peripheral Is Powered Off.")
+    @unknown default:
+      print("Error")
+    }
+  }
+}
+
+extension ViewController: CBPeripheralDelegate {
+}
+
